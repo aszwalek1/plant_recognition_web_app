14976499a8da16edf028cb17f56893bdf3f289cc
Samuel Cameron <scameron3@sheffield.ac.uk>
2024-03-25 15:00:04 +0000

    adds plants controller with create function

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,36 @@
 var express = require('express');
 var router = express.Router();
 
+let plants = require('../controllers/plants')
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Plant Recognition' });
 });
+
+// TODO remove temp test function below
+router.get('/temp', function(req, res, next) {
+  let resultPromise = plants.create(
+    {
+      name: "hi",
+      description: "hi hi",
+      characteristics: {
+        hasFlowers: true,
+        hasLeaves: true,
+        hasFruit: true,
+        hasSeeds: true,
+        sunExposure: "full",
+      },
+      nameStatus: "in-progress",
+      username: "username",
+      date: Date.now(),
+    },
+    "")
+
+  resultPromise.then(result => console.log(result))
+
+  res.render('index', { title: 'temp database test' });
+})
 
 module.exports = router;
