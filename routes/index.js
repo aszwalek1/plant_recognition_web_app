b7e9554b1dde7942e2860ef167fe6ca97cf9c81c
Samuel Cameron <scameron3@sheffield.ac.uk>
2024-04-22 17:20:32 +0100

    adds get all plants function in plants controller

--- routes/index.js
+++ routes/index.js
@@ -1,42 +1,16 @@
 var express = require('express');
 var router = express.Router();
 
-let plants = require('../controllers/plants')
-
+let plantsController = require('../controllers/plants')
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Plant Recognition' });
+router.get('/', function (req, res, next) {
+    let plantsPromise = plantsController.getAll()
+
+    plantsPromise.then(plantsStr => {
+        let plants = JSON.parse(plantsStr)
+        res.render('index', {title: 'Plant Recognition', plants: plants})
+    })
 });
-
-// TODO remove temp test function below
-router.get('/temp', function(req, res, next) {
-  let resultPromise = plants.create(
-    {
-      name: "hi",
-      description: "hi hi",
-      characteristics: {
-        hasFlowers: true,
-        hasLeaves: true,
-        hasFruit: true,
-        hasSeeds: true,
-        sunExposure: "full",
-        height: 10,
-        spread: 5,
-        colour: "#e66465"
-      },
-      nameStatus: "in-progress",
-      username: "username",
-      date: Date.now(),
-    },
-    ""
-  )
-
-  resultPromise.then(result => {
-      console.log(result);
-  })
-
-  res.render('index', { title: 'temp database test' });
-})
 
 module.exports = router;
