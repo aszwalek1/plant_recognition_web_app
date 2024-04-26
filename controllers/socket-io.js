44bc117f5a456827537de2477a410260b40e3170
Alicja Szwalek <93785980+aszwalek1@users.noreply.github.com>
2024-04-26 15:06:21 +0100

    makes changes suggested in the pull request

--- controllers/socket-io.js
+++ controllers/socket-io.js
@@ -2,34 +2,6 @@ const Plant = require("../models/plant");
 
 exports.init = function(io) {
     io.on('connection', function(socket) {
-        // socket.on('join chat', function(userId) {
-        //     console.log('User joined the chat:', userId);
-        //     io.emit('joined', userId);
-        // });
-        //
-        // socket.on('chat', async function(plantId, message, username) {
-        //     console.log("chat")
-        //     try {
-        //         const plant = await Plant.findOneAndUpdate(
-        //             { _id: plantId },
-        //             { $push: { chatMessages: { message, username, datetime: new Date()}}},
-        //             { new: true }
-        //         );
-        //
-        //         if (!plant) {
-        //             console.error('Plant not found');
-        //             return;
-        //         }
-        //         console.log('Plant found:', plant)
-        //         io.emit('chat', username, message);
-        //     } catch (error) {
-        //         console.error('Error saving message:', error);
-        //     }
-        // });
-        // socket.on('disconnect', function() {
-        //     console.log('A user disconnected');
-        //
-        // });
         try {
             socket.on('join chat', function(userId) {
                 console.log('joined chat: ', userId);
