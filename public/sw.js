2cc41babb78e6864c8f58c9ab3b93012004316e1
Alicja Szwalek <93785980+aszwalek1@users.noreply.github.com>
2024-05-09 10:17:54 +0100

    adds sw registration in the head

--- public/sw.js
+++ public/sw.js
@@ -2,32 +2,63 @@ console.log('Service Worker Called...');
 
 self.addEventListener('install', (event) => {
     console.log('[Service Worker] : Installed');
-    event.waitUntil(
-        caches.open('core').then((cache) => {
-            return cache.addAll([
-                '/create'
-            ]);
-        })
-    );
-});
+    event.waitUntil((async () => {
 
+        console.log('Service Worker: Caching App Shell at the moment......');
+        try {
+            const cache = await caches.open("static");
+            cache.addAll([
+                '/',
+                '/create',
+                // '/manifest.json',
+                // '/javascripts/insert.js',
+                // '/javascripts/index.js',
+                // '/javascripts/idb-utility.js',
+                '/stylesheets/style.css'
+                // '/images/image_icon.png',
+            ]);
+            console.log('Service Worker: App Shell Cached');
+        }
+        catch{
+            console.log("error occured while caching...")
+        }
+
+    })());
+});
 self.addEventListener('activate', (event) => {
     console.log('[Service Worker] : Activated');
-});
-
-self.addEventListener('fetch', (event) => {
-    console.log('[Service Worker]: Fetching');
-    const request = event.request;
-    if (request.url.includes('/create')) {
-        event.respondWith(
-            fetch(request).catch(() => {
-                return caches.match(request).then((cachedResponse) => {
-                    return cachedResponse || fetch(request);
-                });
+    event.waitUntil(
+        (async () => {
+            const keys = await caches.keys();
+            return keys.map(async (cache) => {
+                if(cache !== "static") {
+                    console.log('Service Worker: Removing old cache: '+cache);
+                    return await caches.delete(cache);
+                }
             })
-        );
-    } else {
-        event.respondWith(fetch(request));
-    }
+        })()
+    )
 });
 
+
+self.addEventListener('fetch', event => {
+    event.respondWith((async () => {
+        const cache = await caches.open("static");
+        const cachedResponse = await cache.match(event.request);
+        if (cachedResponse) {
+            console.log('Service Worker: Fetching from Cache: ', event.request.url);
+            return cachedResponse;
+        }
+        console.log('Service Worker: Fetching from URL: ', event.request.url);
+        return fetch(event.request);
+    })());
+});
+
+// self.addEventListener('sync', event => {
+//     if(event.tag === 'sync-create') {
+//         console.log('Syncing Created Post');
+//         openSyncPostsDB().then((syncPostDB) => {
+//             getAllSyncPosts(syncPostDB).then((syncPosts))
+//         })
+//     }
+// });
