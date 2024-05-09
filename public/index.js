51967456fdcb3905fd2221ff22a3bbcf20bf59e2
Alicja Szwalek <93785980+aszwalek1@users.noreply.github.com>
2024-05-09 10:17:54 +0100

    adds sw registration in the head

--- public/index.js
+++ public/index.js
@@ -1,6 +1,16 @@
 navigator.serviceWorker.register('/sw.js')
     .then((registration) => {
         console.log('SW registered: ', registration.scope);
+        navigator.serviceWorker.ready
     },(registrationError) => {
         console.log('SW registration failed: ', registrationError);
     });
+
+// if(navigator.online) {
+//     fetch('https://localhost:3000/create')
+//         .then(function(res) {
+//             return res.json();
+//         }).then(function(newPost) {
+//             openIDB
+//     })
+// }
