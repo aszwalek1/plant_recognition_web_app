8fc896899693ae51c46f3b304e5ae3dfe66a8841
Alicja Szwalek <93785980+aszwalek1@users.noreply.github.com>
2024-05-16 15:00:20 +0100

    display plants online in-progress

--- public/idb-index.js
+++ public/idb-index.js
@@ -1,30 +1,51 @@
 
-// const insertPostInList = (post) => {
-//     if (post.text) {
-//         const copy = document.getElementById("post_template").cloneNode()
-//         copy.removeAttribute("id") // otherwise this will be hidden as well
-//         copy.innerText = post.text
-//         copy.setAttribute("data-post-id", post.id)
-//
-//         // Insert sorted on string text order - ignoring case
-//         const postlist = document.getElementById("post_list")
-//         const children = postlist.querySelectorAll("li[data-post-id]")
-//         let inserted = false
-//         for (let i = 0; (i < children.length) && !inserted; i++) {
-//             const child = children[i]
-//             const copy_text = copy.innerText.toUpperCase()
-//             const child_text = child.innerText.toUpperCase()
-//             if (copy_text < child_text) {
-//                 postlist.insertBefore(copy, child)
-//                 inserted = true
-//             }
-//         }
-//         if (!inserted) { // Append child
-//             postlist.appendChild(copy)
-//         }
-//     }
-// }
-//
+
+function createPosts(db, newPosts) {
+    const postsList = document.getElementById("posts-grid")
+    // for (const postListElem of Array.from(postsList)) {
+    for(const plant of newPosts) {
+        const copy = document.getElementById("post-template").cloneNode(true);
+        copy.hidden = false;
+        copy.removeAttribute("id");
+        copy.querySelector(".image").src = plant.imagePath;
+        copy.querySelector(".name").textContent = plant.name;
+        copy.querySelector(".details").href = `/plant/${plant._id}`;
+        copy.querySelector("#location_").value = plant.location;
+        copy.querySelector("#plant-location_").textContent = plant.location;
+
+        let date = new Date(plant.date)
+        copy.querySelector(".date").textContent = date.toLocaleString()
+        copy.querySelector(".status").textContent = plant.nameStatus
+
+        postsList.appendChild(copy)
+    }
+    // }
+}
+    // if (post.text) {
+    //     const copy = document.getElementById("post_template").cloneNode()
+    //     copy.removeAttribute("id") // otherwise this will be hidden as well
+    //     copy.innerText = post.text
+    //     copy.setAttribute("data-post-id", post.id)
+    //
+    //     // Insert sorted on string text order - ignoring case
+    //     const postlist = document.getElementById("post_list")
+    //     const children = postlist.querySelectorAll("li[data-post-id]")
+    //     let inserted = false
+    //     for (let i = 0; (i < children.length) && !inserted; i++) {
+    //         const child = children[i]
+    //         const copy_text = copy.innerText.toUpperCase()
+    //         const child_text = child.innerText.toUpperCase()
+    //         if (copy_text < child_text) {
+    //             postlist.insertBefore(copy, child)
+    //             inserted = true
+    //         }
+    //     }
+    //     if (!inserted) { // Append child
+    //         postlist.appendChild(copy)
+    //     }
+    // }
+
+
 // Register service worker to control making site work offline
 
 window.onload = function () {
@@ -63,12 +84,19 @@ window.onload = function () {
             });
         }
     }
+    fetch('http://localhost:3000/plants')
+        .then(function (res) {
+            console.log("fetch");
+            return res.json();
+        }).then(function (newPosts) {
+        openPostsIDB().then((db) => {
+            createPosts(db, newPosts)
+            deleteAllExistingPostsFromIDB(db).then(() => {
+                addPost(db, newPosts).then(() => {
+                    console.log("All new posts added to IDB")
+                })
+            });
+        });
+    })
 
-    // if (!navigator.onLine) {
-    //     openPostsIDB().then(() => {
-    //         addPostToIDB(plantData).then(() => {console.log("Added to IDB successfully");});
-    //     }).catch(err => {
-    //         console.error("error adding plant to IDB: ", err);
-    //     });
-    // }
 }
