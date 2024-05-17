5d63cc71de0bfc0c2c5a8dfde94fdc661ad245c7
Alicja Szwalek <93785980+aszwalek1@users.noreply.github.com>
2024-05-17 12:47:08 +0100

    resolve conflict

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
+var createError = require('http-errors');
 var logger = require('morgan');
 var bodyParser = require('body-parser');
+
 var indexRouter = require('./routes/index');
 var plantRouter = require('./routes/plant');
 var createRouter = require('./routes/create');
@@ -17,10 +17,11 @@ app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
+app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(bodyParser.urlencoded({extended: false}));
+
 app.use('/', indexRouter);
 app.use('/plant/', plantRouter);
 app.use('/create/', createRouter);
@@ -43,7 +44,7 @@ app.use(function (err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error', {title: 'Error'});
+  res.render('error', { title: 'Error' });
 });
 
 module.exports = app;
